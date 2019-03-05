f6d827d8429a628bbfc8779073da8ea93e0f7b61
Minh Luc Van <minhlv@nccsoft.vn>
2019-03-05 13:58:28 +0700

    update pattern

--- adder-service/index.js
+++ adder-service/index.js
@@ -1,3 +1,9 @@
+function adder () {
+  this.add({ms: 'adder'}, function (msg, respond) {
+    respond(null, { result: Number(msg.number1) + Number(msg.number2)});
+  });
+}
+
 require('seneca')()
-  .use('adder')
+  .use(adder)
   .listen();
--- server/app.js
+++ server/app.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const greeterService = require('./services/greeter');
-const adderService = require('./services/adder');
-const join = require("bluebird").join;
 
 const app = express();
 
 // query params: ?name=Alex&number1=10&number2=26
 app.get('/', function (req, res) {
-    join(
-        greeterService.sayHello(req.query.name),
-        adderService.sum(req.query.number1, req.query.number2),
-        function (greeting, sum) {
-            res.send(`${greeting.result}, sum of numbers is ${sum.result}`);
-        }
-    );
+    seneca.client({ host: 'adder-service' }).act({ms: 'adder'}, function (err, result) {
+        res.send({
+            err: err,
+            result: result
+        });
+    });
 });
 
 module.exports = app;
