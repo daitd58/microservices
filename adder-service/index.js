207eeb8bc0da7a2abb52544ad1a7c634345f39a8
Minh Luc Van <minhlv@nccsoft.vn>
2019-03-05 15:52:12 +0700

    update string pattern

--- adder-service/index.js
+++ adder-service/index.js
@@ -1,5 +1,5 @@
 function adder () {
-  this.add({ms: 'adder'}, function (msg, respond) {
+  this.add('ms:adder', function (msg, respond) {
     console.log('msg', msg);
     respond(null, { result: Number(msg.number1) + Number(msg.number2)});
   });
